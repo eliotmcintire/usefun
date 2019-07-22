1858a994b186a9446f1d2e2581383fc6e7d64c7c
Tati Micheletti <tati.micheletti@gmail.com>
2019-07-22 22:36:22 +0000

    fixing string in substrBoth

--- R/substrBoth.R
+++ R/substrBoth.R
@@ -1,6 +1,6 @@
 #' substrBoth get a sub-string based on the number of characters and the side to start
 #'
-#' @param string String from which to grab a subset
+#' @param strng String from which to grab a subset
 #' @param howManyCharacters numeric. How many characters should be returned in the sub-string?
 #' @param fromEnd logical. Default is TRUE. Should te subset start in the end of the string?
 #'
@@ -11,8 +11,8 @@
 #'
 #' @rdname substrBoth
 
-substrBoth <- function(string, howManyCharacters, fromEnd = TRUE){
-  if (fromEnd) return(substr(x = string, start = nchar(string) - howManyCharacters+1, nchar(string))) else
-  return(substr(x = string, start = 1, stop = nchar(string)-string))
+substrBoth <- function(strng, howManyCharacters, fromEnd = TRUE){
+  if (fromEnd) return(substr(x = strng, start = nchar(strng) - howManyCharacters+1, nchar(strng))) else
+  return(substr(x = strng, start = 1, stop = nchar(strng)-strng))
 }
 
