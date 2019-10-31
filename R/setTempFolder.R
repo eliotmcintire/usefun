a232a0cf903f9ede7312c0fa89e8a337dd9506c0
Tati Micheletti <tati.micheletti@gmail.com>
2019-10-31 20:30:38 +0000

    added emcintir as user to avoid messages

--- R/setTempFolder.R
+++ R/setTempFolder.R
@@ -1,29 +1,29 @@
 #' This functions sets a temporary folder for raster and other packages inside the user's cache folder.
 #' Make sure you understand the consequences of using it.
 #'
-#' @param paths paths set with \code{SpaDES.core::setPaths()}.      
-#' @param setTmpFolder logical. Should the function reset your temp folder to a new location? 
+#' @param paths paths set with \code{SpaDES.core::setPaths()}.
+#' @param setTmpFolder logical. Should the function reset your temp folder to a new location?
 #' @param name caracter. User name for a final verification. If you are sure you want to set
 #'             your temporary location, use "Tati".
-#' 
+#'
 #' @return invisible
 #'
 #' @author Tati Micheletti
 #' @export
 #' @importFrom reproducible asPath checkPath
-#' 
+#'
 #' @rdname setTempFolder
-#' 
+#'
 setTempFolder <- function(paths, setTmpFolder, usr){
   # Set a storage project folder
   require("reproducible")
   workDirectory <- getwd()
   message("Your current temporary directory is ", tempdir())
-  
+
   # Make a temporary folder for downloading files
   answer <- "NO"
   if (setTmpFolder == TRUE){
-    if (usr %in% c("tmichele", "Tati")) {
+    if (usr %in% c("tmichele", "Tati", "emcintir")) {
       answer <- "YES"
     } else {
       answer <- readline(paste0("[ ATTENTION: ] \nYou are changing you temporary folder to \n",
@@ -33,7 +33,7 @@ setTempFolder <- function(paths, setTmpFolder, usr){
     if (answer == "YES") {
       unlink(file.path(paths$cachePath, "tmp"), recursive = TRUE, force = TRUE)
       tempFolder <- asPath(reproducible::checkPath(file.path(paths$cachePath, "tmp"), create = TRUE))
-      
+
       # Set a temporary folder
       if (Sys.info()['sysname'] == "Windows"){
         write(paste0("TMPDIR = '", tempFolder, "'"), file = file.path(Sys.getenv('R_USER'), '.Renviron'))
@@ -45,7 +45,7 @@ setTempFolder <- function(paths, setTmpFolder, usr){
     } else {
       message("Temporary folder setting was CANCELED by the user")
     }
-  } 
+  }
   if (setTmpFolder == TRUE & answer == "YES")
   message(paste0("You changed your temporary folder location,",
                     "\n If you are using Windows, please restart",
