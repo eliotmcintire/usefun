cd0754cd1ca418d4fc27a4755a9bb9d046198b4e
Tati Micheletti <tati.micheletti@gmail.com>
2019-10-31 21:20:51 +0000

    bug fix argument helpers

--- R/helpersBirds.R
+++ R/helpersBirds.R
@@ -1,7 +1,7 @@
 # helpers for bootstrapPercentChanges
 
-.calculateSignificantChangesInBirds <- function(folder, years, species = NULL, pixelBased = TRUE,
-                                               sampleSize = "auto", redFactorTimes = 15, 
+.calculateSignificantChangesInBirds <- function(dataPath, years, species = NULL, pixelBased = TRUE,
+                                               sampleSize = "auto", redFactorTimes = 15,
                                                studyArea = NULL, repetition = NULL){ # limited to 2 years!
   if (is.null(species)){
     spFiles <- grepMulti(x = list.files(folder), patterns = c(years[1],".tif"))
@@ -32,8 +32,8 @@
         dtForTestloc <- dtForTest[location == locality,]
         t <- Sys.time()
         mod <- .calculatePvalueOfRasters(dtForTest = dtForTestloc,
-                                        pixelBased = pixelBased, sampleSize = sampleSize, 
-                                        species = species, 
+                                        pixelBased = pixelBased, sampleSize = sampleSize,
+                                        species = species,
                                         redFactorTimes = redFactorTimes,  bird = bird)
         message(crayon::white("Finished calculations for ", bird,
                               " (locality ", locality, " repetition ", repetition,") ",
@@ -43,15 +43,15 @@
       names(byLocationModelList) <- uniqueLocations
       dt <- data.table::rbindlist(lapply(uniqueLocations, function(loc){
         direction <- ifelse(byLocationModelList[[loc]]$p.value>=0.05,"no change",
-                            ifelse(byLocationModelList[[loc]]$mean1 < byLocationModelList[[loc]]$mean2, 
+                            ifelse(byLocationModelList[[loc]]$mean1 < byLocationModelList[[loc]]$mean2,
                                    "increased","decreased"))
-        dt <- data.table::data.table(species = bird, tTest = byLocationModelList$p.value, 
+        dt <- data.table::data.table(species = bird, tTest = byLocationModelList$p.value,
                                      result = direction, location = loc)
       }))
     } else {
-      r <- .calculatePvalueOfRasters(dtForTest = dtForTest, 
-                                    pixelBased = pixelBased, sampleSize = sampleSize, 
-                                    species = species, 
+      r <- .calculatePvalueOfRasters(dtForTest = dtForTest,
+                                    pixelBased = pixelBased, sampleSize = sampleSize,
+                                    species = species,
                                     redFactorTimes = redFactorTimes, bird = bird)
       direction <- ifelse(r$p.value>=0.05,"no change",
                           ifelse(r$mean1 < r$mean2, "increased","decreased"))
@@ -69,7 +69,7 @@
       changesVector <- table(changesTable[location == locality, ..column,])
       dt <- data.table::data.table(direction = names(changesVector),
                                    value = as.numeric(changesVector),
-                                   percent = 100*(as.numeric(changesVector)/NROW(changesTable[location == locality, ..column,])), 
+                                   percent = 100*(as.numeric(changesVector)/NROW(changesTable[location == locality, ..column,])),
                                    location = locality)
     }))
   } else {
@@ -109,11 +109,11 @@
         increased <- lapply(listOfChangesLocal, '[[', "increased")
         decreased <- lapply(listOfChangesLocal, '[[', "decreased")
         noChange <- lapply(listOfChangesLocal, '[[', "noChange")
-        
+
         increasedConsistent <- Reduce(intersect, increased)
         decreasedConsistent <- Reduce(intersect, decreased)
         noChangeConsistent <- Reduce(intersect, noChange)
-        
+
         return(list(increased = increasedConsistent,
                     decreased = decreasedConsistent,
                     noChange = noChangeConsistent))
@@ -125,11 +125,11 @@
     increased <- lapply(listOfChanges, '[[', "increased")
     decreased <- lapply(listOfChanges, '[[', "decreased")
     noChange <- lapply(listOfChanges, '[[', "noChange")
-    
+
     increasedConsistent <- Reduce(intersect, increased)
     decreasedConsistent <- Reduce(intersect, decreased)
     noChangeConsistent <- Reduce(intersect, noChange)
-    
+
     return(list(increased = increasedConsistent, decreased = decreasedConsistent, noChange = noChangeConsistent))
   }
 }
@@ -142,8 +142,8 @@
   return(dt)
 }
 
-.calculatePvalueOfRasters <- function(dtForTest, pixelBased = FALSE, 
-                                      sampleSize, species, redFactorTimes, 
+.calculatePvalueOfRasters <- function(dtForTest, pixelBased = FALSE,
+                                      sampleSize, species, redFactorTimes,
                                       bird){
   if (pixelBased){
     if (!is.null(sampleSize)){
@@ -151,11 +151,11 @@
         # COMPUTE IDEAL SIZE SAMPLE FOR SAMPLING USING Cohen's D And Hedges G Effect Size
         treatment <- dtForTest[[2]][!is.na(dtForTest[[2]])]
         control <- dtForTest[[1]][!is.na(dtForTest[[1]])]
-        
+
         ## data and factor
-        cohen <- cohen.d(treatment, control, paired = TRUE, 
+        cohen <- cohen.d(treatment, control, paired = TRUE,
                          conf.level = 0.01, hedges.correction = TRUE)
-        limit <- 0.2 
+        limit <- 0.2
         # Effect size	d	Reference
         # Very small	0.01	Sawilowsky, 2009
         # Small	      0.20	Cohen, 1988
@@ -166,15 +166,15 @@
         vectorSize <- length(treatment)
         message(crayon::yellow("Calculating ideal sample size using Cohen's D and Hedges'g effect size statistics..."))
         while (cohen$estimate < limit){
-          newSample <- sample(x = 1:NROW(treatment), 
-                              size = vectorSize, 
+          newSample <- sample(x = 1:NROW(treatment),
+                              size = vectorSize,
                               replace = FALSE)
-          cohen <- cohen.d(treatment[newSample], control[newSample], paired = TRUE, 
+          cohen <- cohen.d(treatment[newSample], control[newSample], paired = TRUE,
                            conf.level = 0.01, hedges.correction = TRUE)
           vectorSize <- vectorSize - round(vectorSize/redFactorTimes, 0)
         }
         sampleSize <- length(newSample)
-        message(crayon::green(paste0("Ideal sample size calculated as ", 
+        message(crayon::green(paste0("Ideal sample size calculated as ",
                                      sampleSize, "\n with sample size effect calcutated as")))
         print(cohen)
       }
@@ -183,11 +183,11 @@
       env <- environment()
       if (!exists("sbsetID")){
         if (tryCatch({
-          pryr::where(name = "sbsetID", env = env) 
-          return(FALSE)}, 
+          pryr::where(name = "sbsetID", env = env)
+          return(FALSE)},
           error = function(e) return(TRUE))){
-          assign(x = "sbsetID", value = sample(x = dtForTest[!is.na(dtForTest)[ID]]$ID, 
-                                               size = sampleSize, replace = FALSE), 
+          assign(x = "sbsetID", value = sample(x = dtForTest[!is.na(dtForTest)[ID]]$ID,
+                                               size = sampleSize, replace = FALSE),
                  envir = env)
         } else {
           sbsetID <- get("sbsetID", envir = pryr::where(name = "sbsetID", env = env))
@@ -195,7 +195,7 @@
       }
       dtForTest <- dtForTest[sbsetID,]
     }
-    test <- wilcox.test(x = dtForTest[[1]], y = dtForTest[[2]], 
+    test <- wilcox.test(x = dtForTest[[1]], y = dtForTest[[2]],
                         paired = TRUE, alternative = "two.sided")
     p.value <- test$p.value
     mean1 <- mean(dtForTest[[1]], na.rm = TRUE)
@@ -207,9 +207,9 @@
     mean2 <- mean(dtForTest[[2]], na.rm = TRUE)
     sd2 <- sd(dtForTest[[2]], na.rm = TRUE)
     sampleSize2 <- sum(!is.na(dtForTest[[2]]))
-    test <- .tTestMeansSD(mean1 = mean1, mean2 = mean2, sd1 = sd1, sd2 = sd2, 
+    test <- .tTestMeansSD(mean1 = mean1, mean2 = mean2, sd1 = sd1, sd2 = sd2,
                          sampleSize1 = sampleSize1, sampleSize2 = sampleSize2)
-    
+
     p.value <- test["pValue"]
   }
   return(list(mean1 = mean1, mean2 = mean2, p.value = p.value))
@@ -226,43 +226,43 @@
   return(location)
 }
 
-.tTestMeansSD <- function(mean1, mean2, sd1, sd2, 
-                         sampleSize1, sampleSize2, m0 = 0, 
+.tTestMeansSD <- function(mean1, mean2, sd1, sd2,
+                         sampleSize1, sampleSize2, m0 = 0,
                          equal.variance = FALSE){
   # FROM Macro: https://stats.stackexchange.com/questions/30394/how-to-perform-two-sample-t-tests-in-r-by-inputting-sample-statistics-rather-tha
   # mean1, mean2: the sample means
   # sd1, sd2: the sample standard deviations
   # sampleSize1, sampleSize2: the same sizes
-  # m0: the null value for the difference in means to be tested for. Default is 0. 
-  # equal.variance: whether or not to assume equal variance. Default is FALSE. 
-  
+  # m0: the null value for the difference in means to be tested for. Default is 0.
+  # equal.variance: whether or not to assume equal variance. Default is FALSE.
+
   if(equal.variance == FALSE ){
     se <- sqrt( (sd1^2/sampleSize1) + (sd2^2/sampleSize2) )
     # welch-satterthwaite df
     df <- ( (sd1^2/sampleSize1 + sd2^2/sampleSize2)^2 )/( (sd1^2/sampleSize1)^2/(sampleSize1-1) + (sd2^2/sampleSize2)^2/(sampleSize2-1) )
   } else {
     # pooled standard deviation, scaled by the sample sizes
-    se <- sqrt( (1/sampleSize1 + 1/sampleSize2) * ((sampleSize1-1)*sd1^2 + (sampleSize2-1)*sd2^2)/(sampleSize1+sampleSize2-2) ) 
+    se <- sqrt( (1/sampleSize1 + 1/sampleSize2) * ((sampleSize1-1)*sd1^2 + (sampleSize2-1)*sd2^2)/(sampleSize1+sampleSize2-2) )
     df <- sampleSize1 + sampleSize2 - 2
-  }      
-  t <- (mean1 - mean2 - m0)/se 
-  dat <- c(mean1-mean2, se, t, 2*pt(-abs(t),df))    
+  }
+  t <- (mean1 - mean2 - m0)/se
+  dat <- c(mean1-mean2, se, t, 2*pt(-abs(t),df))
   names(dat) <- c("DifferenceOfMeans", "stdError", "t", "pValue")
-  return(dat) 
+  return(dat)
 }
 
 .calcICPercentChange <- function(percChange){
   increasedIC <- tryCatch({
-    t.test(percChange[direction == "increased", value], conf.level = 0.95)$conf.int}, 
+    t.test(percChange[direction == "increased", value], conf.level = 0.95)$conf.int},
     error = function(e) return(NA))
   decreasedIC <- tryCatch({
-    t.test(percChange[direction == "decreased", value], conf.level = 0.95)$conf.int}, 
+    t.test(percChange[direction == "decreased", value], conf.level = 0.95)$conf.int},
     error = function(e) return(NA))
   noChangeIC <- tryCatch({
-    t.test(percChange[direction == "no change", value], conf.level = 0.95)$conf.int}, 
+    t.test(percChange[direction == "no change", value], conf.level = 0.95)$conf.int},
     error = function(e) return(NA))
   dt <- data.table::data.table(direction = c("increased", "decreased", "noChange"),
-                               lower95 = c(increasedIC[1], decreasedIC[1], noChangeIC[1]), 
+                               lower95 = c(increasedIC[1], decreasedIC[1], noChangeIC[1]),
                                upper95 = c(increasedIC[2], decreasedIC[2], noChangeIC[2]))
   return(dt)
 }
