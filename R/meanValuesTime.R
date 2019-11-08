c33145562448f13087acc730f505ffb6fdbaca9b
Tati Micheletti <tati.micheletti@gmail.com>
2019-11-08 06:37:30 +0000

    browswers

--- R/meanValuesTime.R
+++ R/meanValuesTime.R
@@ -20,9 +20,9 @@ meanValuesTime <- function(ras,
   # scenario == "LandR.CS_fS" | "LandR_fS" | "LandR_SCFM" | "LandR.CS_SCFM"
   if (is(ras, "RasterStack")){
     fullTable <- lapply(names(ras), FUN = function(year){
-      modTable <- lapply(ras[[year]], FUN = function(mod){
-        if (is(mod, "list")){
-          meanAndUnc <- lapply(mod, function(eachRas){
+      eachRasToCalcTable <- lapply(ras[[year]], FUN = function(eachRasToCalc){
+        if (is(eachRasToCalc, "list")){
+          meanAndUnc <- lapply(eachRasToCalc, function(eachRas){
             average <- median(eachRas[], na.rm = TRUE)
             rasType <- ifelse(grepl(names(eachRas), pattern = "Uncertain"), "SD", "AVERAGE")
             yr <- as.numeric(substrBoth(strng = names(eachRas), howManyCharacters = nchar(initialTime)))
@@ -34,11 +34,12 @@ meanValuesTime <- function(ras,
           dt$IC <- dt$SD*1.96
           return(dt)
         } else {
-          rasType <- ifelse(grepl(names(mod), pattern = "Uncertain"), "SD", "AVERAGE")
+          browser()
+          rasType <- ifelse(grepl(names(eachRasToCalc), pattern = "Uncertain"), "SD", "AVERAGE")
           t1 <- Sys.time()
-          Mean <- mean(mod[], na.rm = TRUE)
-          Median <- median(mod[], na.rm = TRUE)
-          yr <- as.numeric(substrBoth(strng = names(mod), howManyCharacters = nchar(initialTime[1]), fromEnd = TRUE))
+          Mean <- mean(eachRasToCalc[], na.rm = TRUE)
+          Median <- median(eachRasToCalc[], na.rm = TRUE)
+          yr <- as.numeric(substrBoth(strng = names(eachRasToCalc), howManyCharacters = nchar(initialTime[1]), fromEnd = TRUE))
           dt <- data.table::data.table(average = Mean, Median = Median,
                                        year = yr, scenario = scenario,
                                        rasType = rasType)
@@ -51,14 +52,14 @@ meanValuesTime <- function(ras,
         browser()
         })
       browser()
-      return(rbindlist(modTable))
+      return(rbindlist(eachRasToCalcTable))
     })
     fullTable <- rbindlist(fullTable)
     return(fullTable)
   } else {
     fullTable <- lapply(ras, FUN = function(year){
-      modTable <- lapply(year, FUN = function(mod){
-        meanAndUnc <- lapply(mod, function(eachRas){
+      eachRasToCalcTable <- lapply(year, FUN = function(eachRasToCalc){
+        meanAndUnc <- lapply(eachRasToCalc, function(eachRas){
           average <- median(eachRas[], na.rm = TRUE)
           rasType <- ifelse(grepl(names(eachRas), pattern = "Uncertain"), "SD", "AVERAGE")
           yr <- as.numeric(substrBoth(string = names(eachRas), howManyCharacters = nchar(initialTime)))
@@ -70,7 +71,7 @@ meanValuesTime <- function(ras,
         dt$IC <- dt$SD*1.96
         return(dt)
       })
-      return(rbindlist(modTable))
+      return(rbindlist(eachRasToCalcTable))
     })
     fullTable <- rbindlist(fullTable)
     return(fullTable)
