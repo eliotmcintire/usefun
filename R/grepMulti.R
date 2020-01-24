222ca41bb04451a38f96876e61e4a40a6b7addcc
Tati Micheletti <tati.micheletti@gmail.com>
2020-01-24 10:33:39 -0800

    Improved grepMulti to accept unwanted patterns in the search

--- R/grepMulti.R
+++ R/grepMulti.R
@@ -3,14 +3,23 @@
 #' @param x object where to look for patterns.
 #'
 #' @param patterns Character vector of patterns to look for objects.
-#'                
+#'
+#' @param unwanted Character vector of patterns to exclude from search.
+#'
 #' @return The objects with specified patterns combined
-#' 
+#'
 #' @author Tati Micheletti
 #' @export
 #' @rdname grepMulti
 
-grepMulti <- function(x, patterns) {
+grepMulti2 <- function(x, patterns, unwanted = NULL) {
   rescued <- sapply(x, function(fun) all(sapply(X = patterns, FUN = grepl, fun)))
-  return(x[rescued])
+  recovered <- x[rescued]
+  if (!is.null(unwanted)){
+    discard <- sapply(recovered, function(fun) all(sapply(X = unwanted, FUN = grepl, fun)))
+    afterFiltering <- recovered[!discard]
+    return(afterFiltering)
+  } else {
+    return(recovered)
+  }
 }
